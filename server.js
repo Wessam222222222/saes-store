089a71dbfdb5fa5c5f4588e8d7c41d3a622375a1
wessam developer <161109058+Wessam222222222@users.noreply.github.com>
2024-07-04 20:20:16 +0400

    Add files via upload
    Signed-off-by: wessam developer <161109058+Wessam222222222@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,45 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
 const app = express();
-const port = 3000;
+const port = 4000; // تغيير المنفذ إلى 4000
 
-// Replace <password> with your MongoDB password
-const uri = "mongodb+srv://wessamahmedsamirahmed:<password>@cluster0.qoyhumj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Middleware لتقديم الملفات الثابتة مثل HTML، CSS، JS
+app.use(express.static('public'));
 
-let db;
-
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database');
-    db = client.db('ads-database');
-  })
-  .catch(error => console.error(error));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/api/ads', (req, res) => {
-  const ad = req.body;
-  db.collection('ads').insertOne(ad)
-    .then(result => {
-      res.status(201).send(result);
-    })
-    .catch(error => {
-      res.status(500).send(error);
-    });
+// إعداد مسار رئيسي
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/api/ads', (req, res) => {
-  db.collection('ads').find().toArray()
-    .then(results => {
-      res.status(200).send(results);
-    })
-    .catch(error => {
-      res.status(500).send(error);
-    });
-});
-
+// بدء السيرفر
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
